aec798f02ea3eed2e6fc5fd05d37bf0259784f68
Irina Scortar <irinascortar@gmail.com>
2023-04-26 02:48:42 +0300

    DONE

--- backend/src/main/java/info/scortar/irina/dogsdatabase/DogsDatabaseApplication.java
+++ backend/src/main/java/info/scortar/irina/dogsdatabase/DogsDatabaseApplication.java
@@ -2,9 +2,10 @@ package info.scortar.irina.dogsdatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DogsDatabaseApplication {
+public class DogsDatabaseApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DogsDatabaseApplication.class, args);
